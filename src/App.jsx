2433bc6c6f6c658e7781b53d1a5443464b257da9
christopheluc62 <christopheluc62@gmail.com>
2021-05-11 15:52:36 +0200

    Add a component Quote

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './components/Header/Header';
+import Quote from './components/Quote/Quote';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Quote />
     </div>
   );
 }
---
+++ src/components/Quote/QuoteCss.jsx
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+const QuoteCss = styled.div``;
+
+export default QuoteCss;
