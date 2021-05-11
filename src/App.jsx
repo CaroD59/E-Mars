4ce637d81efad8a1080df04e00e349b3a4137115
christopheluc62 <christopheluc62@gmail.com>
2021-05-11 16:23:36 +0200

    App modification

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,14 @@
+import React from 'react';
 import Footer from './components/Footer/Footer';
 import ContactForm from './components/ContactForm/ContactForm';
-import React from 'react';
 import Header from './components/Header/Header';
-
-
 
 function App() {
   return (
     <div className="App">
-
-      
       <Header />
       <ContactForm />
-    <Footer />
-   
-
-
+      <Footer />
     </div>
   );
 }
