582b263b915b5b2f17ca6c9eb2c153f820c18aa6
christopheluc62 <christopheluc62@gmail.com>
2021-05-11 19:15:06 +0200

    add a component Quote

--- src/components/Quote/Quote.jsx
+++ src/components/Quote/Quote.jsx
@@ -4,19 +4,22 @@ import QuoteCss from './QuoteCss';
 function Quote() {
   return (
     <QuoteCss>
-      <div>
-        <h2>The Three Laws of Robotics:</h2>
+      <div className="image">
+        <img src="background2.png" alt="background" />
+      </div>
+      <div className="quote">
+        <h2>The Three Laws of Robotics</h2>
         <p>
           1: A robot may not injure a human being or, through inaction, allow a
-          human being to come to harm;
+          human being to come to harm
         </p>
         <p>
           2: A robot must obey the orders given it by human beings except where
-          such orders would conflict with the First Law;
+          such orders would conflict with the First Law
         </p>
         <p>
           3: A robot must protect its own existence as long as such protection
-          does not conflict with the First or Second Law;
+          does not conflict with the First or Second Law
         </p>
         <p>Isaac Asimov, I, Robot</p>
       </div>
