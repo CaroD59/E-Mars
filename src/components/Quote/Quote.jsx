4209942fe664d5f7c421841c562a5170d96e2fc2
CaroD59 <dorchies.c@gmail.com>
2021-05-12 08:59:39 +0200

    a little bit of CSS

--- src/components/Quote/Quote.jsx
+++ src/components/Quote/Quote.jsx
@@ -4,24 +4,26 @@ import QuoteCss from './QuoteCss';
 function Quote() {
   return (
     <QuoteCss>
-      <div className="image">
-        <img src="background2.png" alt="background" />
-      </div>
-      <div className="quote">
-        <h2>The Three Laws of Robotics</h2>
-        <p>
-          1: A robot may not injure a human being or, through inaction, allow a
-          human being to come to harm
-        </p>
-        <p>
-          2: A robot must obey the orders given it by human beings except where
-          such orders would conflict with the First Law
-        </p>
-        <p>
-          3: A robot must protect its own existence as long as such protection
-          does not conflict with the First or Second Law
-        </p>
-        <p>Isaac Asimov, I, Robot</p>
+      <div className="QuoteContainer">
+        <div className="image">
+          <img src="background2.png" alt="background" />
+        </div>
+        <div className="quote">
+          <h1 className="titlequote">The Three Laws of Robotics</h1>
+          <p className="p-quote">
+            1/ A robot may not injure a human being or, through inaction, allow
+            a human being to come to harm
+          </p>
+          <p className="p-quote">
+            2/ A robot must obey the orders given it by human beings except
+            where such orders would conflict with the First Law
+          </p>
+          <p className="p-quote">
+            3/ A robot must protect its own existence as long as such protection
+            does not conflict with the First or Second Law
+          </p>
+          <p>Isaac Asimov, I, Robot</p>
+        </div>
       </div>
     </QuoteCss>
   );
--- src/components/Quote/QuoteCss.jsx
+++ src/components/Quote/QuoteCss.jsx
@@ -1,18 +1,37 @@
 import styled from 'styled-components';
 
 const QuoteCss = styled.div`
-  display: flex;
-  text-align: center;
-  margin: auto;
-  height: 220px;
-  width: 85%;
-  margin-top: 15px;
-  border-spacing: 50px;
+  .QuoteContainer {
+    display: flex;
+    text-align: center;
+    margin: auto;
+    height: 220px;
+    width: 85%;
+    margin-top: 15px;
+    border-spacing: 50px;
+  }
 
   .quote {
+    font-style: italic;
+    padding-top: 20px;
+    margin-top: 20px;
     margin-left: 40px;
     font-family: 'Roboto', sans-serif;
+    height: auto;
     font-size: 15px;
+    border-radius: 30px;
+    text-align: left;
+  }
+
+  .titlequote {
+    font-weight: bold;
+    text-align: center;
+    font-size: 20px;
+    margin-bottom: 20px;
+  }
+
+  .p-quote {
+    margin-bottom: 20px;
   }
 `;
 
