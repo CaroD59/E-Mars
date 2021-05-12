f01da7959dd6bae44d5774c7acfac99b36af5ba3
yulen59 <j.delemarle@gmail.com>
2021-05-12 10:01:45 +0200

    commit before pull

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import styled from 'styled-components';
-import React from 'react';
+
 import { Reset } from 'styled-reset';
 import ContactForm from './components/ContactForm/ContactForm';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Quote from './components/Quote/Quote';
 import MessageGallery from './components/MessageGallery';
-
 
 export default function App() {
   const StyleApp = styled.div`
