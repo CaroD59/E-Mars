51ad0fc13dc24eec9283226d1210696a8357e046
Loredan <davidloredanvicentiu@gmail.com>
2021-05-12 09:52:32 +0200

    changed all data

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import styled from 'styled-components';
-import React from 'react';
 import { Reset } from 'styled-reset';
 import ContactForm from './components/ContactForm/ContactForm';
 import Header from './components/Header/Header';
@@ -9,11 +8,10 @@ import Footer from './components/Footer/Footer';
 import Quote from './components/Quote/Quote';
 import MessageGallery from './components/MessageGallery';
 
-
 export default function App() {
   const StyleApp = styled.div`
-    background-color: #ebecf0;
-    margin: 0;
+  background - color: #ebecf0;
+  margin: 0;
   `;
   return (
     <StyleApp>
